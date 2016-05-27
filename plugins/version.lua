30f42d2b6f042e66a1b1814ec71e808803f268a7
raminea <raminea4240@gmail.com>
2016-05-27 20:12:07 +0430

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,39 @@
+
+do
+
+function run(msg, matches)
+  return [[ 
+⛓Tele Mandy version 1.0 🔸
+⛓➖➖➖➖➖
+⛓Based on TeleSeed & Telegram-bot 🔹
+⛓➖➖➖➖➖
+⛓user name of sudo: @raminea 🔸
+⛓➖➖➖➖➖
+⛓sudo id: 195092846 🔹
+⛓➖➖➖➖➖
+⛓phone number of sudo: +17722002353 🔸
+⛓➖➖➖➖➖
+⛓github account: github.com/raminea/TeleMandy0113 🔹
+⛓➖➖➖➖➖
+⛓special thanks to saeed & ali 🔸
+⛓➖➖➖➖➖
+⛓username of bot: https://telegram.me/TeleMandy 🔹
+⛓➖➖➖➖➖
+⛓bot id: 212899738🔸
+⛓➖➖➖➖➖
+⛓phone number of bot: +13306675118 🔹
+⛓➖➖➖➖➖
+⛓powered by: RaminYaghi 🔸
+]]
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "telemandy: Shows bot version",
+  patterns = {
+    "^[/!]([Tt]elemandy)$"
+  }, 
+  run = run 
+}
+
+end
