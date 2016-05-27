7b7d9dfc396ad4675ea86539c490b8470ab34eb4
raminea <raminea4240@gmail.com>
2016-05-27 18:11:07 +0430

    Update TeleMandy0113.lua

--- bot/TeleMandy0113.lua
+++ bot/TeleMandy0113.lua
@@ -235,7 +235,8 @@ function create_config( )
     "echo",
     "fantasy_writer",
     "feedback",
-    "file_manager"
+    "file_manager",
+    "filtering"
     },
     sudo_users = {195092846},--Sudo users
     disabled_channels = {},
