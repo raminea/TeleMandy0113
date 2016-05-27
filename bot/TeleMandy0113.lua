27bf68beaabece5c8c95dd5a309b7706271dd303
raminea <raminea4240@gmail.com>
2016-05-27 17:18:47 +0430

    Update TeleMandy0113.lua

--- bot/TeleMandy0113.lua
+++ bot/TeleMandy0113.lua
@@ -228,7 +228,8 @@ function create_config( )
     "addplug",
     "addsudo",
     "superlogo",
-    "block"
+    "block",
+    "boobs"
     },
     sudo_users = {195092846},--Sudo users
     disabled_channels = {},
