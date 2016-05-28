5020da0157daafad6d293acb35b17d1326317cc0
raminea <raminea4240@gmail.com>
2016-05-28 14:05:51 +0430

    Create rmsg.lua

---
+++ plugins/rmsg.lua
@@ -0,0 +1,31 @@
+local function history(extra, suc, result)
+  for i=1, #result do
+    delete_msg(result[i].id, ok_cb, false)
+  end
+  if tonumber(extra.con) == #result then
+    send_msg(extra.chatid, '"'..#result..'" ✅پیام اخیر سوپر گروه حذف شد', ok_cb, false)
+  else
+    send_msg(extra.chatid, '✅️تعداد پیام مورد نظر شما پاک شد', ok_cb, false)
+  end
+end
+local function run(msg, matches)
+  if matches[1] == 'rmsg' and is_owner(msg) then
+    if msg.to.type == 'channel' then
+      if tonumber(matches[2]) > 10000 or tonumber(matches[2]) < 1 then
+        return "تعداد بیشتر از 1 مجاز است"
+      end
+      get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
+    else
+      return "⚠️️فقط در سوپرگروه ممکن است"
+    end
+  else
+    return "🖕🏻😐️شما دسترسی ندارید"
+  end
+end
+
+return {
+    patterns = {
+        '^[/#](rmsg) (%d*)$'
+    },
+    run = run
+}
